042cf76f25180541ca32a9eaaaeb652cf6e98bbe
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-24 22:40:31 +0900

    Initial commit

---
+++ src/main/java/love/chihuyu/arenum/Arenum.kt
@@ -0,0 +1,13 @@
+package love.chihuyu.arenum
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Arenum : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
